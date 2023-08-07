40fbfa793929f7674e2cf61d59f161f179cfde61
Влад Киселев <kam2010-76@mail.ru>
2023-08-07 17:01:24 +0300

    add csv upload, upload in db, companies page

--- web/app.py
+++ web/app.py
@@ -1,15 +1,75 @@
+import csv
 import os
 
-from flask import Flask
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///companies.db'
 app.config['SECRET_KEY'] = os.urandom(10)
+app.config["DEBUG"] = True
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET'])
+class Companies(db.Model):
+    ticker = db.Column(db.String(30), primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    sector = db.Column(db.String(80), nullable=False)
+    ebitda = db.Column(db.Float, nullable=True)
+    sales = db.Column(db.Float, nullable=True)
+    net_profit = db.Column(db.Float, nullable=True)
+    market_price = db.Column(db.Float, nullable=True)
+    net_debt = db.Column(db.Float, nullable=True)
+    assets = db.Column(db.Float, nullable=True)
+    equity = db.Column(db.Float, nullable=True)
+    cash_equivalents = db.Column(db.Float, nullable=True)
+    liabilities = db.Column(db.Float, nullable=True)
+
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+
+
+class UploadForm(FlaskForm):
+    csv_file = FileField('CSV File', validators=[DataRequired()])
+    submit = SubmitField('Upload')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index_page():
-    return 'Hello'
+    form = UploadForm()
+    if form.validate_on_submit():
+        csv_file = form.csv_file.data
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'],
+                                 csv_file.filename)
+        csv_file.save(file_path)
+        with open(file_path, 'r') as file:
+            data = list(csv.DictReader(file))
+            for dic in data:
+                for column_name, row_value in dic.items():
+                    if row_value == '':
+                        dic[column_name] = None
+                if not db.session.query(Companies).filter_by(
+                        ticker=Companies(**dic).ticker).first():
+                    db.session.add(Companies(**dic))
+            db.session.commit()
+        os.remove(file_path)
+        flash("CSV файл успешно загружен!")
+    # else:
+    #     flash("Неверный тип файла")
+    return render_template('index.html', form=form)
+
+
+@app.route('/companies', methods=['GET'])
+def companies_page():
+    companies_query = db.session.query(Companies).all()
+    return render_template('companies.html', query=companies_query)
 
 
 if __name__ == '__main__':
